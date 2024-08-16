dd8385ee91915976f52378e59624004b363f892c
AerixBd <drutopaybd@gmail.com>
2024-08-16 12:48:51 +0600

    Create payment_methods.sql

---
+++ payment_methods.sql
@@ -0,0 +1,2 @@
+INSERT INTO `payment_methods` (`id`, `method_name`, `method_get`, `method_min`, `method_max`, `method_type`, `method_extras`, `method_line`) VALUES
+(71, 'drutopay', 'drutopay', 1, 1000, '2', '{\"method_type\":\"1\",\"name\":\"drutopay\",\"min\":\"1\",\"max\":\"10000\",\"api_key\":\"12345\",\"secret_key\":\"12345\",\"brand_key\":\"12345\",\"currency_rate\":\"100\"}', 1);
